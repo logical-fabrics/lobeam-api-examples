db6b3e3739fbba9c0392af13647db23c22b91013
Koki_Kubota <39072144+KokiKubota2@users.noreply.github.com>
2020-07-14 05:06:29 +0900

    arrived

---
+++ httpCallout/routes/arrived.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+const faker = require('faker')
+const jwt = require('jsonwebtoken')
+const express = require('express')
+const router = express.Router()
+const argv = require('yargs')
+  .usage('usage: $0 <command> [options]')
+  .alias('u', 'url')
+  .nargs('u', 1)
+  .describe('u', 'URL')
+  .alias('p', 'passphrase')
+  .nargs('p', 1)
+  .describe('u', 'passphrase')
+  .demandOption(['u', 'p']).argv
+
+const { basePayload } = require('../lib/constants')
+
+const url = argv.u
+const passphrase = argv.p
+
+/* GET users listing. */
+router.get('/', async (req, res) => {
+  const payload = {
+    ...basePayload,
+    status: 'ARRIVED',
+    eventType: 'SESSION UPDATE',
+    arrivedAt: faker.date.recent(),
+  }
+  res.send(payload)
+
+  const token = jwt.sign(payload, passphrase)
+  await axios.post(url, token)
+})
+
+module.exports = router
