34dff41be07b08eb7ff005f38ab6c71d8c68a3ad
raynux <ray.sakai@logical-fabrics.com>
2020-08-02 20:19:33 +0900

    Update app.js

--- httpCallout/app.js
+++ httpCallout/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const jwt = require('jsonwebtoken')
+const fs = require('fs')
 
 const app = express()
 
@@ -10,28 +11,28 @@ app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.text())
 
-// この値を LoBeam 上で設定されたパスフレーズに書き換えてください
-const PASSPHRASE = 'YOUR_PASSPHRASE'
+// LoBeam 上で設定されたパスフレーズを環境変数 PHASSPHASE に設定してください
+const PASSPHRASE = process.env.PASSPHRASE || ''
+const FILE_PATH = process.env.OUTPUT_PATH || './callout.log'
 
 //
 // HTTP Callout Endpoint
 //
 app.post('/', async (req, res) => {
+  let decoded
   try {
-    const decoded = jwt.verify(req.body, PASSPHRASE) // パスフレーズの検証
+    decoded = jwt.verify(req.body, PASSPHRASE) // パスフレーズの検証
     console.log(decoded)
-    res.json({
-      result: 'OK',
-      decoded,
-      posted: req.body,
-    })
   } catch (e) {
     console.log('検証失敗： パスフレーズが一致しません')
-    res.status(400).json({
+    return res.status(400).json({
       result: 'NG',
       posted: req.body,
     })
   }
+
+  fs.appendFileSync(FILE_PATH, JSON.stringify(decoded) + '\n', 'utf8')
+  res.json({ result: 'OK' })
 })
 
 // catch 404 and forward to error handler
